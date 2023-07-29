6364dc5cf7b40090ec070fe46748780ecdb4b816
Wadad Parker <wadadparker5@gmail.com>
2023-07-29 13:18:13 +0530

    feat: sidebar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import styles from "./home.module.css";
 
+import { SideBar } from "../../components/sideBar/SideBar";
+
 export const Home=()=>
 {
     return (
-        <h1>This is Home</h1>
+        <div className={styles[`page-container`]}>
+            <SideBar />    
+            <h1>This is Home</h1>
+        </div>
     )
 }
